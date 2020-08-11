01828a75294ff619bfa2659fb8ecba6c1ebd5738
Vir Bedi <virbedi98@gmail.com>
2020-08-10 23:37:51 -0700

    Updated .gitignore, added pod for blue check mark and other task updates

---
+++ notesApp/ToDoList/Task.swift
@@ -0,0 +1,9 @@
+//
+//  Task.swift
+//  notesApp
+//
+//  Created by Vir Bedi on 8/8/20.
+//  Copyright © 2020 Vir Bedi. All rights reserved.
+//
+
+import Foundation
---
+++ notesApp/ToDoList/TaskCell.swift
@@ -0,0 +1,24 @@
+//
+//  TaskCell.swift
+//  notesApp
+//
+//  Created by Vir Bedi on 8/9/20.
+//  Copyright © 2020 Vir Bedi. All rights reserved.
+//
+
+import UIKit
+
+class TaskCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
